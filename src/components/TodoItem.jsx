dd975fd21dda4737fcd6f94ddbd8c428c88d4336
Swayam Shrestha <swayams24@gmail.com>
2024-09-04 00:06:14 +1000

    Add minimal styling to when task is marked complete

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,10 +4,23 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
+
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, status: !todo.status } : todo
+      )
+    );
+  }
+
+  const className = item.status ? styles.completed : "";
+
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button
             onClick={() => handleDelete(item)}

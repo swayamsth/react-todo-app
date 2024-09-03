159a181bbeec1d4020f1f676afc2328911dc33ce
Swayam Shrestha <swayams24@gmail.com>
2024-09-03 22:45:15 +1000

    Divide components

---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Form({ todos, setTodos }) {
+  const [todo, setTodo] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input
+        onChange={(e) => setTodo(e.target.value)}
+        type="text"
+        value={todo}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,27 +1,14 @@
 import { useState } from "react";
-import TodoItem from "./TodoItem";
+import Form from "./Form";
+import TodoList from "./Todolist";
 
 export default function Todo() {
-  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo("");
-  }
   return (
     <div>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          onChange={(e) => setTodo(e.target.value)}
-          type="text"
-          value={todo}
-        />
-        <button type="submit">Add</button>
-      </form>
-      {todos.map((item) => (
-        <TodoItem key={item} item={item} />
-      ))}
+      <Form todos={todos} setTodos={setTodos} />
+
+      <TodoList todos={todos} />
     </div>
   );
 }
